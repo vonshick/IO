9ed5e8b73d8caa377eb7ff8faf93a687ae75d674
vonshick <kubawasik@vp.pl>
2018-10-30 15:53:37 +0100

    Pierwsza zmiana

--- Program.java
+++ Program.java
@@ -1,8 +1,9 @@
 public class FreshJuiceTest {
-
+//
    public static void main(String args[]) {
 	System.out.println("Change"); 
       FreshJuice juice = new FreshJuice();
+	        System.out.println("Change"); 
       juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
 	        System.out.println("Change"); 
       System.out.println("Size: " + juice.size);
