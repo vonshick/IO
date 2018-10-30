221dc0618f3147a032125a9dc7afcc07a4b868fe
vonshick <kubawasik@vp.pl>
2018-10-30 15:25:07 +0100

    Java file added

---
+++ Program.java
@@ -0,0 +1,8 @@
+public class FreshJuiceTest {
+
+   public static void main(String args[]) {
+      FreshJuice juice = new FreshJuice();
+      juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
+      System.out.println("Size: " + juice.size);
+   }
+}
