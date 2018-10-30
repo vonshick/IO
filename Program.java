4828ed3b5d00071298001c423113bf0f608b4cc5
vonshick <kubawasik@vp.pl>
2018-10-30 15:51:31 +0100

    Partial change

--- Program.java
+++ Program.java
@@ -1,8 +1,10 @@
 public class FreshJuiceTest {
 
    public static void main(String args[]) {
+	System.out.println("Change"); 
       FreshJuice juice = new FreshJuice();
       juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
+	        System.out.println("Change"); 
       System.out.println("Size: " + juice.size);
       System.out.println("Git testing");  
  }
